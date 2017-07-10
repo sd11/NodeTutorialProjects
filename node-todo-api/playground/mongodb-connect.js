413ae61ca98beb48857a231699904eefc685d89f
Shatabdi Dey <Shatabdi.Dey@mi9.com.au>
2017-07-10 15:48:52 +1000

    uncommented code that was written for creating a collection and inserting a document

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -7,30 +7,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
-    // db.collection('Todos').insertOne({
-    //     text: 'Somthn to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert todo', err);
-    //     }
-    //     //JSON pretty print function. Undefined for filter and 2 for indentation.
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
+    db.collection('Todos').insertOne({
+        text: 'Somthn to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        //JSON pretty print function. Undefined for filter and 2 for indentation.
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
 
-    // Insert new document into users (name, age, location)
-    // db.collection('Users').insertOne({
-    //     name: 'Shaon',
-    //     age: 24,
-    //     location: 'Mct'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert users', err);
-    //     }
-    //     //JSON pretty print function. Undefined for filter and 2 for indentation.
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    //     console.log((result.ops[0]._id).getTimestamp());
-    // });
+    Insert new document into users (name, age, location)
+    db.collection('Users').insertOne({
+        name: 'Shaon',
+        age: 24,
+        location: 'Mct'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert users', err);
+        }
+        //JSON pretty print function. Undefined for filter and 2 for indentation.
+        console.log(JSON.stringify(result.ops, undefined, 2));
+        console.log((result.ops[0]._id).getTimestamp());
+    });
     db.close();
 });
 
