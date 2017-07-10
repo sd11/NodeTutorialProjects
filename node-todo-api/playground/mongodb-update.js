06ef6b240e6e1b234eb4a6fe3575ecc390410933
Shatabdi Dey <Shatabdi.Dey@mi9.com.au>
2017-07-10 16:38:46 +1000

    update db

---
+++ node-todo-api/playground/mongodb-update.js
@@ -0,0 +1,18 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    db.collection('Users').findOneAndUpdate({name: 'Harish'}, {
+        $set: {
+            age: 30
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+});
