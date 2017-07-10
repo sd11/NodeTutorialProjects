90d549d0510774c10812be803a27901c72a326ae
Shatabdi Dey <Shatabdi.Dey@mi9.com.au>
2017-07-10 15:47:36 +1000

    add a delete script

---
+++ node-todo-api/playground/mongodb-delete.js
@@ -0,0 +1,25 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    //delete many
+    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+        console.log(result);
+    });
+
+    //delete one
+    db.collection('Todos').deleteOne({text: 'Kill the monster'}).then((result) => {
+        console.log(result);
+    });
+
+    //find and delete
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log(result);
+    });
+});
+
